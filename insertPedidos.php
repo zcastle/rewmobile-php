5e9e4f372610e806123107274512e53bd9d8fe8f
JC <jzcastle@gmail.com>
2014-03-26 09:34:52 -0500

    fix inserPedidos.php

--- insertPedidos.php
+++ insertPedidos.php
@@ -2,13 +2,13 @@
 require_once 'bootstrap.php';
 //date_default_timezone_set('America/Lima');
 $result = array();
-if ($_POST) {
+if ($_POST["data"]) {
     try {
-        $data = json_decode($_REQUEST["data"]);
+        $data = json_decode($_POST["data"]);
         $result['success'] = true;
 
         $tot=$data->precio*$data->cant;
-        $prod=Producto::find($data->idprod)->no_producto;
+        //$prod=Producto::find($data->idprod)->no_producto;
 
         $curdate = date('Y-m-d');
         $curtime = date('H:i:s');
@@ -18,19 +18,19 @@ if ($_POST) {
             'usuario'=>$data->cajeroid,
             'mozo'=>$data->mozoid,
             'idproducto'=>$data->idprod,
-            'producto'=>$prod,
+            'producto'=>$data->producto,
             'cantidad'=>$data->cant,
             'precio'=>$data->precio,
             'fecha'=>$curdate,
             'hora'=>$curtime,
             'pax'=>$data->pax,
             'total'=>$tot,
-            'pc'=>'TABLET',
+            'pc'=>$data->pc,
             'co_destino'=>$data->co_destino,
             'stado'=> 0
         );
 
-        $condicion=array('conditions' => array('nroatencion=? AND idproducto=? AND producto=? AND fl_envio LIKE ?', $data->nmesa, $data->idprod, $prod, '%'));
+        $condicion=array('conditions' => array('nroatencion=? AND idproducto=? AND producto=?', $data->mesa, $data->idprod, $data->producto));
         $atencion=Atenciones::first($condicion);
         if($atencion){
             if($atencion->fl_envio=='N'){
@@ -38,27 +38,29 @@ if ($_POST) {
                 $total=$atencion->precio*$cant;
                 $atencion->update_attributes(array('cantidad'=>$cant, 'total'=>$total));
                 $result['message']='Producto no enviado, se actualiza la cantidad';
-                $result['idatencion']=0;
+                $result['id']=0;
             } else {
                 $r=Atenciones::create($attributes);
                 $result['message']='Producto enviado, se ingresa nuevo registro';
-                $result['idatencion']=$r->idatencion;
+                $result['id']=$r->idatencion;
             }
         } else {
             $ca=CabeceraAtencion::first(array('conditions' => array('n_ate', $data->nmesa)));
             $ca->update_attributes(array('fl_sta'=>1));
             $r=Atenciones::create($attributes);
             $result['message']='Nuevo registro';
-            $result['idatencion']=$r->idatencion;
+            $result['id']=$r->idatencion;
         }
     } catch (ActiveRecordException $e) {
         $result['success']=false;
         $result['message']='Ha ocurrido algun error';
+        $result['id']=0;
     }
     print_r(json_encode($result));
 } else {
     $result['success']=false;
     $result['message']='Ha ocurrido algun error :P';
+    $result['id']=0;
     print_r(json_encode($result));
 }
 ?>
